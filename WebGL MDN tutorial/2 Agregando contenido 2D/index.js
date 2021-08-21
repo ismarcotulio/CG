2acc3b9007db5241bb4cb2a4b9cfc630dde22741
Marco Ruiz <martulioruiz@gmail.com>
2021-08-21 16:47:22 -0600

    Shader functions
    1. A function was implemented to load shaders.
    2. A function was implemented to initialize the shaderProgram

---
+++ WebGL MDN tutorial/2 Agregando contenido 2D/index.js
@@ -0,0 +1,62 @@
+
+function main(){
+    //OBTENIENDO EL SELECTOR DEL CANVAS
+    const canvas = document.querySelector("#glCanvas")
+
+    //INICIALIZANDO EN CONTEXTO DE LENGUAJE GRAFICO WEBGL
+    const gl = canvas.getContext("webgl");
+
+    //COMRPOBANDO QUE WEBGL TRABAJA CORRECTAMENTE
+    if(gl === null){
+        alert( "No se pudo inicializar WEBGL, comprueba que tu navegador o pc lo soporten" )
+        return;
+    }
+
+    //ASIGNAMOS UN LIMPIADO DE PANTALLA EN NEGRO SIN OPACIDAD
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    //LIMPIAMOS EN COLOR EN EL BUFFER CON EL COLOR ESPECIFICADO, EN ESTE CASO NEGRO
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+}
+//ESTA FUNCION INICIALIZA NUESTROS SHADERS PARA QUE WEBGL SEPA COMO DIBUJAR NUESTROS DATOS
+function initShaderProgram(gl, vsSource, fsSource){
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FREGMENT_SHADER, fsSource);
+
+    //CREANDO EL PROGRAMA SHADER
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    //VERIFICANDO QUE EL PROGRAMA SHADER SE HALLA CREADO CORRECTAMENTE
+    if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)){
+        alert(`No se ah podido inicializar el programa shader ${gl.getShaderInfoLog(shaderProgram)}`);
+        return null
+    }
+
+    return shaderProgram;
+}
+
+//ESTA FUNCIOM CREA UN SHADER DADO UN TIPO, CARGA EL ARCHIVO FUENTE Y LO COMPILA
+function loadShader( gl, type, source ){
+    const shader = gl.createShader(type);
+
+    //ENVIANDO EL ARCHIVO FUENTE HACIA EL OBJETO SHADER
+    gl.shaderSource(shader, source);
+
+    //COMPILANDO EL PROGRAMA SHADER
+    gl.compileShader(shader);
+
+    //VERIFICANDO QUE EL PROGRAMA SE HAYA COMPILADO EXITOSAMENTE
+    if (!getShaderParameter(shader, gl.COMPILE_STATUS)){
+        alert(`Ah ocurrido un error al tratar de compilar el shader ${gl.getShaderInfoLog(shader)}`);
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
+
+window.onload = main
